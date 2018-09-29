e1341088c71402705c3c383dd2a3728507e86545
admin <admin@stu7753c3>
2018-09-29 19:09:15 +0800

    day01

---
+++ src/main/java/com/bwf/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.bwf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
+public class UserController {
+	@RequestMapping("a")
+	public String test(){
+		System.out.println("ok");
+		return "test/a";
+	}
+
+}
